93aee27a4f44d583fa19a33e4a9a2bc613667553
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-06-15 11:41:46 -0500

    Modified mail in User class to be checked by regex

--- store/models/user.py
+++ store/models/user.py
@@ -1,16 +1,16 @@
 from pydantic import BaseModel
 import enum
+from fastapi import Query
 
 
 class Role(enum.Enum):
-    admin = "admin"
-    personal = "personal"
+    admin: str = "admin"
+    personal: str = "personal"
 
 
 class User(BaseModel):
     name: str
     password: str
-    mail: str
+    #Validate mail format with regex, could change ... for adefault value
+    mail: str = Query(..., regex="^([a-zA-Z0-9\-\.]+)@([a-zA-Z0-9\-\.]+)\.([a-zA-Z]{2,5})$")
     role: Role
-
-
