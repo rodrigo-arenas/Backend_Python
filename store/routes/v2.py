7829215b82dc7c728bc8fb189d1d8c78463debf6
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-06-17 20:55:25 -0500

    Added endpoints tags

--- store/routes/v2.py
+++ store/routes/v2.py
@@ -5,6 +5,6 @@ from starlette.status import HTTP_201_CREATED
 app_v2 = APIRouter()
 
 
-@app_v2.post('/user', status_code=HTTP_201_CREATED)
+@app_v2.post('/user', status_code=HTTP_201_CREATED,  tags=["User"])
 async def post_user(user: User):
     return {"request body": user, "api version": 2}
