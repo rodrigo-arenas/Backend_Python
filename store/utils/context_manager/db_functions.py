545ba2cb47e7075aedcb855998d503cfd39ae68d
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-06-22 22:47:49 -0500

    Renamed function to be db_get_user and changed return to be db_user

--- store/utils/context_manager/db_functions.py
+++ store/utils/context_manager/db_functions.py
@@ -1,15 +1,11 @@
 from utils.context_manager.db_connection import DatabaseConnection
 from utils.constants import DB_HOST, DB_NAME, DB_USER, DB_PASSWORD
-import asyncio
 
 
-async def check_jwt_user(user):
+async def db_get_user(user):
     async with DatabaseConnection(host=DB_HOST, port=5432,
                                   database=DB_NAME, user=DB_USER,
                                   password=DB_PASSWORD) as connection:
-        values = await connection.fetch(f"SELECT * FROM public.fn_get_user('{user}')")
-        print(values)
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(check_jwt_user('user1'))
+        query = f"SELECT * FROM public.fn_get_user('{user.username}')"
+        db_user = dict(await connection.fetchrow(query))
+        return db_user
