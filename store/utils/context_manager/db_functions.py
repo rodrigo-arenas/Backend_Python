5d41728e43fae0029271fe8b4e3512811cc9cfd0
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-06-23 23:53:14 -0500

    Added db_check_jwt_username function

--- store/utils/context_manager/db_functions.py
+++ store/utils/context_manager/db_functions.py
@@ -9,3 +9,27 @@ async def db_get_user(user):
         query = f"SELECT * FROM public.fn_get_user('{user.username}')"
         db_user = dict(await connection.fetchrow(query))
         return db_user
+
+
+async def db_check_jwt_username(username):
+    async with DatabaseConnection(host=DB_HOST, port=5432,
+                                  database=DB_NAME, user=DB_USER,
+                                  password=DB_PASSWORD) as connection:
+        query = f"SELECT * FROM public.fn_get_user('{username}')"
+        db_user = dict(await connection.fetchrow(query))
+        if bool(db_user):
+            return True
+        else:
+            return False
+
+
+async def db_insert_personnel(user):
+    async with DatabaseConnection(host=DB_HOST, port=5432,
+                                  database=DB_NAME, user=DB_USER,
+                                  password=DB_PASSWORD) as connection:
+        query = f"SELECT * FROM public.fn_insert_personnel(\
+                                '{user.name}', \
+                                '{user.password}', \
+                                '{user.mail}', \
+                                '{user.role}')"
+        await connection.execute(query)
