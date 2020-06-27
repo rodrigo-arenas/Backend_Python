0d9b4957dee8e93dab12362e957189325fe7e937
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-06-27 11:13:19 -0500

    Moved the connection definition out of the file

--- store/utils/context_manager/db_functions.py
+++ store/utils/context_manager/db_functions.py
@@ -1,9 +1,4 @@
-from utils.context_manager.db_connection import DatabaseConnection
-from utils.constants import DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT
-
-db_connection = DatabaseConnection(host=DB_HOST, port=DB_PORT,
-                                   database=DB_NAME, user=DB_USER,
-                                   password=DB_PASSWORD)
+from utils.context_manager.db_object import db_connection
 
 
 async def db_get_user(user):
