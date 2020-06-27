4c7d80ba9d5f9cd04b08eeb1d4bc552480336523
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-06-27 11:14:33 -0500

    Defined redis connection db according to environment

--- store/utils/context_manager/redis_object.py
+++ store/utils/context_manager/redis_object.py
@@ -1,1 +1,12 @@
+import aioredis
+from utils.constants import TEST, REDIS_URL, TEST_REDIS_URL
+
 redis = None
+
+
+async def redis_connection():
+    if TEST:
+        redis_db = await aioredis.create_redis_pool(REDIS_URL)
+    else:
+        redis_db = await aioredis.create_redis_pool(TEST_REDIS_URL)
+    return redis_db
