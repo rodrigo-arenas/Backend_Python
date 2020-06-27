338c1d4f933b8d46010079b98c67bd9c967d04bb
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-06-27 11:15:34 -0500

    Created database queries for testing api

---
+++ store/tests/set_database.py
@@ -0,0 +1,30 @@
+import asyncio
+from utils.context_manager.db_object import db_connection
+
+loop = asyncio.get_event_loop()
+
+
+async def insert_user(username, password):
+    async with db_connection as connection:
+        query = f"""INSERT INTO public.users(username, password) values('{username}', '{password}')"""
+        await connection.execute(query)
+
+
+async def clear_db():
+    async with db_connection as connection:
+        query = """TRUNCATE users, personnel, books, authors;"""
+        await connection.execute(query)
+
+
+async def check_personnel_mail(name, mail):
+    async with db_connection as connection:
+        query = f"""SELECT * FROM personnel WHERE name = '{name}' and mail = '{mail}'"""
+        try:
+            db_user = dict(await connection.fetchrow(query))
+        except Exception as e:
+            db_user = {}
+
+        if bool(db_user):
+            return True
+        else:
+            return False
