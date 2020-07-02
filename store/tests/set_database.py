42a1721ee31085cc605375a3ef17b848c1320883
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-07-01 23:30:53 -0500

    Changed db connection to use pool

--- store/tests/set_database.py
+++ store/tests/set_database.py
@@ -1,20 +1,29 @@
+import asyncio
+import nest_asyncio
+import store.utils.context_manager.db_object as database
 from store.utils.context_manager.db_object import db_connection
+
+nest_asyncio.apply()
+
+loop = asyncio.get_event_loop()
+if not loop.is_running():
+    database.pool = loop.run_until_complete(db_connection())
 
 
 async def insert_user(username, password):
-    async with db_connection as connection:
+    async with database.pool.acquire() as connection:
         query = f"""INSERT INTO public.users(username, password) values('{username}', '{password}')"""
         await connection.execute(query)
 
 
 async def clear_db():
-    async with db_connection as connection:
+    async with database.pool.acquire() as connection:
         query = """TRUNCATE users, personnel, books, authors;"""
         await connection.execute(query)
 
 
 async def check_personnel_mail(name, mail):
-    async with db_connection as connection:
+    async with database.pool.acquire() as connection:
         query = f"""SELECT * FROM personnel WHERE name = '{name}' and mail = '{mail}'"""
         try:
             db_user = dict(await connection.fetchrow(query))
