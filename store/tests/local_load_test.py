765f8877a188d253d1c5a1899eb8fc3fb95fdd12
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-07-02 22:47:07 -0500

    Commented test

--- store/tests/local_load_test.py
+++ store/tests/local_load_test.py
@@ -1,6 +1,6 @@
 import uuid
 from locust import HttpUser, TaskSet, task, between
-from store.tests.endpoints_test import get_auth_header
+
 """
 Run locus with:
 locust -f ./store/tests/local_load_test.py
@@ -8,22 +8,22 @@ locust -f ./store/tests/local_load_test.py
 
 
 class BookstoreLocusTasks(TaskSet):
-    #@task
-    #def token_test(self):
-    #    self.client.post('/token', dict(username='test', password='test'))
-
     @task
-    def post_user_test(self):
-        user_dict = {"username": uuid.uuid4().hex,
-                     "password": uuid.uuid4().hex,
-                     "mail": "mails@mail.com",
-                     "role": "admin"}
-        auth_header = get_auth_header()
-        self.client.post('/v1/user', json=user_dict, headers=auth_header)
+    def token_test(self):
+        self.client.post('/token', dict(username='test', password='test'))
+
+    #@task
+    #def post_user_test(self):
+    #    user_dict = {"username": uuid.uuid4().hex,
+    #                 "password": uuid.uuid4().hex,
+    #                 "mail": "mails@mail.com",
+    #                 "role": "admin"}
+    #    auth_header = {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0Iiwicm9sZSI6ImFkbWluIiwiZXhwIjoxNTkzNzM4MDAzfQ.5sHHslXdGe8PfXBiI_x9VhSsY3igLB2P4HPgv06UZVM'}
+    #    self.client.post('/v1/user', json=user_dict, headers=auth_header)
 
 
 class BookstoreLoadTest(HttpUser):
     tasks = [BookstoreLocusTasks]
     host = 'http://127.0.0.1:5000'
-    wait_time = between(1, 3)
+    wait_time = between(4, 10)
     stop_timeout = 20
