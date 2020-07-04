54c8e8aaec7d280a90793b4dfcf75dadb3d52f98
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-07-03 23:15:38 -0500

    Fixed wrong variable in post /v1/user test

--- store/tests/local_load_test.py
+++ store/tests/local_load_test.py
@@ -8,18 +8,18 @@ locust -f ./store/tests/local_load_test.py
 
 
 class BookstoreLocusTasks(TaskSet):
-    @task
-    def token_test(self):
-        self.client.post('/token', dict(username='test', password='test'))
-
     #@task
-    #def post_user_test(self):
-    #    user_dict = {"username": uuid.uuid4().hex,
-    #                 "password": uuid.uuid4().hex,
-    #                 "mail": "mails@mail.com",
-    #                 "role": "admin"}
-    #    auth_header = {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0Iiwicm9sZSI6ImFkbWluIiwiZXhwIjoxNTkzNzM4MDAzfQ.5sHHslXdGe8PfXBiI_x9VhSsY3igLB2P4HPgv06UZVM'}
-    #    self.client.post('/v1/user', json=user_dict, headers=auth_header)
+    #def token_test(self):
+    #    self.client.post('/token', dict(username='test', password='test'))
+
+    @task
+    def post_user_test(self):
+        user_dict = {"name": uuid.uuid4().hex,
+                     "password": uuid.uuid4().hex,
+                     "mail": "mails@mail.com",
+                     "role": "admin"}
+        auth_header = {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0Iiwicm9sZSI6ImFkbWluIiwiZXhwIjoxNTkzODMxNjg0fQ.H02OUAfwbUTJrJvE0Ps6NJmi9RCmdGjiDAyAytUioJk'}
+        self.client.post('/v1/user', json=user_dict, headers=auth_header)
 
 
 class BookstoreLoadTest(HttpUser):
